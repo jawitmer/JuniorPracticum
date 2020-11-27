42df17290443d14e87293ec6d5c4470688b66968
chloeyano <72216329+chloeyano@users.noreply.github.com>
2020-11-27 18:27:22 -0500

    agg3 app with selectInput for lines

---
+++ appAgg3LineSelection.R
@@ -0,0 +1,70 @@
+
+library(tidyverse)
+library(mosaicData)
+library(shiny)
+df <- read.csv("ShinyPoliceAgg.csv")
+
+# Define UI for application that draws a scatterplot
+ui <- fluidPage(
+    
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("x", label = "X variable",
+                        choices = names(df), selected = "WhitePoliceShare"),
+            selectInput("y", label = "Y variable",
+                        choices = names(df), selected = "RatioBlackWhite"),
+            selectInput("transform", label = "Y transformation",
+                        choices = c("identity", "log", "sqrt"), selected = "identity"),
+            selectInput(inputId = "color", 
+                        label = "Color by:",
+                        choices = c("None", "Region", "animus_score", "implicit_score_mean"),
+                        selected = "None"),
+            selectInput(inputId = "line", 
+                        label = "Add Line?",
+                        choices = c("None", "Line", "Smoother"),
+                        selected = "None",
+                        multiple = FALSE),
+
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a scatterplot
+server <- function(input, output) {
+    
+    dataset <- reactive({
+        transform_fun <-
+            switch(
+                input$transform,
+                identity = function(x) {x},
+                log = function(x) {log(1 + x)},
+                sqrt = function(x) {sqrt(x)}
+            )
+        df %>%
+            mutate(
+                y = transform_fun(.data[[input$y]])
+            )
+    })
+    
+    output$distPlot <- renderPlot({
+        p <- ggplot(dataset(),
+                    aes_string(x = input$x, y = "y", color = input$color)) +
+            geom_point() +
+            labs(y = paste0(input$y, " (", input$transform, ")"))
+        if (input$line == "Line") {
+            p <- p + stat_smooth(method = lm, se=FALSE)
+        }
+        if (input$line == "Smoother") {
+            p <- p + geom_smooth(se=FALSE)
+        }
+        p
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
